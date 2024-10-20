9a15f04fbf9255dd20dc22f5ba349927dc59a0db
Nikola Andreev <andr.nikola.08@gmail.com>
2024-10-20 16:07:42 +0300

    register finally done

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject, signal } from '@angular/core';
 import { AuthStateService } from '../../shared/state/auth.state.service';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
 import { InputHelpers } from '../../shared/helpers/input.helpers';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-register',
@@ -23,16 +24,35 @@ export class RegisterComponent {
     agreesToTerms: [false, Validators.requiredTrue],
   });
 
+  passwordsDoMatch = signal<boolean>(true);
+
   hasSubmitted = signal<boolean>(false);
 
   inputHelpers: InputHelpers;
 
   constructor(private fb: FormBuilder) {
     this.inputHelpers = new InputHelpers(this.registerForm, this.hasSubmitted);
+
+    this.registerForm.valueChanges
+      .pipe(takeUntilDestroyed())
+      .subscribe((val) => {
+        if (val.password === val.confirmPassword) {
+          this.passwordsDoMatch.set(true);
+        } else if (
+          this.hasSubmitted() &&
+          this.registerForm.get('confirmPassword')?.touched &&
+          this.registerForm.get('password')?.touched
+        ) {
+          this.passwordsDoMatch.set(false);
+        }
+      });
   }
 
   onSubmit() {
     console.log(this.registerForm.value);
+
+    if (this.registerForm.invalid) return;
+
     this.hasSubmitted.set(true);
   }
 }
